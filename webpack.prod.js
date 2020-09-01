cb745bc8bc4878df247b6a1b02e4c0154940151f
Matias <matiascasale1@gmail.com>
2020-09-01 18:52:35 -0300

    Primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,72 @@
+const HtmlWebPackPlugin      = require('html-webpack-plugin');
+const MiniCssExtractplugin   = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MinifyPlugin           = require('babel-minify-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    optimization: {
+        minimizer: [ new OptimizeCssAssetsPlugin() ]
+    },
+    output: {  
+        filename: 'main.[contentHash].js'     
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: [
+                    'babel-loader'
+                ] 
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractplugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractplugin({
+            filename: '[name].[contentHash].css',
+            ignoreOrder: false
+        }),
+        new MinifyPlugin(),
+        new CleanWebpackPlugin(),
+        ]
+    }
